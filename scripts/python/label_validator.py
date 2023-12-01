56f0ad80b9e307b6111aaedb01a55be0553a2571
Marek Michali <marek.michali@sap.com>
2023-12-01 12:08:00 +0100

    wip

--- scripts/python/label_validator.py
+++ scripts/python/label_validator.py
@@ -16,12 +16,10 @@ print(f"Label pool: {label_pool}")
 token = os.getenv('GITHUB_TOKEN')
 repo = 'kyma-project/kyma-environment-broker'
 
-
 response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest', headers={'Authorization': f'token {token}'})
 response.raise_for_status()
 latest_release = response.json()
 latest_release_date = latest_release['created_at']
-
 
 response = requests.get(f'https://api.github.com/repos/{repo}/pulls?state=closed&sort=updated&direction=desc', headers={'Authorization': f'token {token}'})
 response.raise_for_status()
@@ -32,27 +30,25 @@ prs_since_last_release = [
     if pr['merged_at'] is not None and pr['merged_at'] > latest_release_date
 ]
 
-
 valid_prs = []
 invalid_prs = []
 for pr in prs_since_last_release:
     labels = [label['name'] for label in pr['labels']]
     common_labels = set(labels).intersection(label_pool)
     if len(common_labels) != 1:
-        invalid_prs.append((pr['number'], pr['html_url']))
+        invalid_prs.append(pr['html_url'])
     else:
-        valid_prs.append(pr['number'])
+        valid_prs.append(pr['html_url'])
 
 
 print("\nThese PRs have exactly one label from the pool:")
 for pr_number in valid_prs:
     print(f"PR #{pr_number}")
 
-
 if invalid_prs:
     print("\nThese PRs don't have exactly one label from the pool:")
     for pr_number, pr_url in invalid_prs:
         print(f"PR #{pr_number}: {pr_url}")
     sys.exit(1) 
-else:
-    print("\nAll PRs have exactly one label from the pool")
+
+print("\nAll PRs have exactly one label from the pool")
