d7512a26a156b06c58dcc2c913acd404cf2ce127
Marek Michali <marek.michali@sap.com>
2024-02-08 13:49:54 +0100

    wip

--- setup-envtest.sh
+++ setup-envtest.sh
@@ -5,3 +5,4 @@ mkdir -p $LOCAL_BIN
 GOBIN=$LOCAL_BIN go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 OUTPUT=$($LOCAL_BIN/setup-envtest use --bin-dir $LOCAL_BIN -p path latest)
 echo $OUTPUT
+ss
